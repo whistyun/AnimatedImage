197976ee9273be70357f1ab7c81805b0168a9989
whistyun <kanadische_goldrute@yahoo.co.jp>
2024-01-22 20:04:18 +0900

    enables to build with net462

--- AnimatedImage.Avalonia/IBitmapSource.cs
+++ AnimatedImage.Avalonia/IBitmapSource.cs
@@ -9,17 +9,47 @@ namespace AnimatedImage.Avalonia
 
     public record BitmapStream : IBitmapSource
     {
-        public Stream StreamSource { get; init; }
+        public Stream StreamSource
+        {
+            get;
+#if NET5_0_OR_GREATER
+            init;
+#endif
+        }
+
+#if NET5_0_OR_GREATER
+        public BitmapStream() { }
+#endif
+
+        public BitmapStream(Stream stream)
+        {
+            StreamSource = stream;
+        }
 
         public static implicit operator BitmapStream(Stream stream)
-            => new BitmapStream { StreamSource = stream };
+            => new BitmapStream(stream);
     }
 
     public record BitmapUri : IBitmapSource
     {
-        public Uri UriSource { get; init; }
+        public Uri UriSource
+        {
+            get;
+#if NET5_0_OR_GREATER
+            init;
+#endif
+        }
+
+#if NET5_0_OR_GREATER
+        public BitmapUri() { }
+#endif
+
+        public BitmapUri(Uri uri)
+        {
+            UriSource = uri;
+        }
 
         public static implicit operator BitmapUri(Uri uri)
-            => new BitmapUri { UriSource = uri };
+            => new BitmapUri(uri);
     }
 }
